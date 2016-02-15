705c59879d024025ef9dc513341c29998648a62f
Olivier Pasquier <olivierpasquier@myrte.sio.ecp.fr>
2016-02-15 14:55:02 +0100

    Version 0.01

--- DataTest/AppDelegate.m
+++ DataTest/AppDelegate.m
@@ -16,7 +16,55 @@
 @implementation AppDelegate
 
 - (void)applicationDidFinishLaunching:(NSNotification *)aNotification {
-    // Insert code here to initialize your application
+
+  
+    NSString *dataUrl = @"http://sio.lab.corp.abelionni.com/api/tweets.json";
+    NSURL *url = [NSURL URLWithString:dataUrl];
+
+    NSURLSessionDataTask *downloadTask = [[NSURLSession sharedSession]
+    dataTaskWithURL:url completionHandler:^(NSData *data, NSURLResponse *response, NSError *error) {
+        //no data received
+    }];
+    
+   [downloadTask resume];
+    
+    
+    // to get data from the Json into a Dictionary
+    
+
+  
+        NSString *responseString = [[NSString alloc] initWithData:responseData encoding:NSUTF8StringEncoding];
+        NSError *e = nil;
+        NSData *jsonData = [responseString dataUsingEncoding:NSUTF8StringEncoding];
+        NSDictionary *JSON = [NSJSONSerialization JSONObjectWithData:jsonData options: NSJSONReadingMutableContainers error: &e];
+    
+    
+    
+    
+    // Methode to post stuff
+    
+    NSURLSessionConfiguration *config = [NSURLSessionConfiguration defaultSessionConfiguration];
+    NSURLSession *session = [NSURLSession sessionWithConfiguration:config];
+    
+    NSMutableURLRequest *request = [[NSMutableURLRequest alloc] initWithURL:url];
+    request.HTTPMethod = @"POST";
+
+    NSDictionary *dictionary = @{@"toto": @"motdepasse"};
+    NSError *error = nil;
+    NSData *data = [NSJSONSerialization dataWithJSONObject:dictionary
+                                                   options:kNilOptions error:&error];
+    
+    if (!error) {
+          NSURLSessionUploadTask *uploadTask = [session uploadTaskWithRequest:request
+                                                                   fromData:data completionHandler:^(NSData *data,NSURLResponse *response,NSError *error) {
+                                                                       // Handle response here
+                                                                   }];
+        
+ 
+        [uploadTask resume];
+    }
+    
+    
 }
 
 - (void)applicationWillTerminate:(NSNotification *)aNotification {
